be788cd2a5614ade260bd987ad2d6848152c4912
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2020-01-22 10:20:27 -0500

    Updates

---
+++ precache-manifest.e9ee5a4a02842bd5ee1c98291ceb0640.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a4d08dbb708342a3ddbfb51ab7a02b",
+    "url": "/reactjs-reward-store/index.html"
+  },
+  {
+    "revision": "a2c25a1d6f7b7dbaee75",
+    "url": "/reactjs-reward-store/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "ca3634f063a74df760b2",
+    "url": "/reactjs-reward-store/static/css/main.3d077b9a.chunk.css"
+  },
+  {
+    "revision": "a2c25a1d6f7b7dbaee75",
+    "url": "/reactjs-reward-store/static/js/2.1e5d7ed7.chunk.js"
+  },
+  {
+    "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
+    "url": "/reactjs-reward-store/static/js/2.1e5d7ed7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca3634f063a74df760b2",
+    "url": "/reactjs-reward-store/static/js/main.5fa71b3d.chunk.js"
+  },
+  {
+    "revision": "d2a521f176ea98a53a98",
+    "url": "/reactjs-reward-store/static/js/runtime-main.02522bdc.js"
+  },
+  {
+    "revision": "41527a03a11700403c22ed3febcf5a1d",
+    "url": "/reactjs-reward-store/static/media/coin.41527a03.gif"
+  }
+]);
